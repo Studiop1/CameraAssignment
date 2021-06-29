5f9f8b63445e629519e755dadefcc708d396d6f2
Studiop1 <85195210+Studiop1@users.noreply.github.com>
2021-06-29 18:22:04 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/git2/MainActivity.kt
+++ app/src/main/java/com/example/git2/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.git2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// This assignment is the assignment to do a simple pizza ordering app.
-// The app uses buttons, text fields and an image.
+// This assignment is the assignment to do an app that uses the phone's camera
+// (First it asks permission to use the camera)
 // You have a youtube video but also a 'shortcut' paper to explain how
 // Watch the video and then follow the steps in the paper ( or just
 // follow the video
